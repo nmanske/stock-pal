1acbdae6e108a0867d52fc2ee204a8c1756c727f
Nathan Manske <nathan.manske@utexas.edu>
2017-09-13 23:35:42 -0500

    Add GET logic and connection info

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,19 @@
 #include <ESP8266WiFi.h>
 #include <SPI.h>
 
-static const char ssid[] = "my_ssid";
-static const char password[] = "my_password";
-static const char host[] = "https://jsonplaceholder.typicode.com";
+void printJSON();
+
+#define BAUD_RATE 115200
+#define HTTP_PORT 80
+
+static const char ssid[] = "router power";
+static const char password[] = "powertotherouter";
+static const char host[] = "jsonplaceholder.typicode.com";
 
 static int status = WL_IDLE_STATUS;
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(BAUD_RATE);
     while (!Serial);
 
     if (WiFi.status() == WL_NO_SHIELD) {
@@ -24,10 +29,42 @@ void setup() {
         delay(10000);
     }
 
-    Serial.println("You're connected to the network");
+    Serial.println("\nWiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("Signal strength: ");
+    Serial.print(WiFi.RSSI());
+    Serial.println(" dbm\n");
 }
 
 void loop() {
     delay(10000);
-    Serial.println(WiFi.status());
+    printJSON();
+}
+
+void printJSON() {
+    Serial.print("Connecting to ");
+    Serial.println(host);
+
+    WiFiClient client;
+    if (!client.connect(host, HTTP_PORT)) {
+        Serial.println("Connection failed");
+        return;
+    }
+
+    String url = "/posts";
+    Serial.print("Requesting URL: ");
+    Serial.print(url);
+
+    client.print(String("GET") + url +
+                 " HTTP/1.1\r\n");
+    delay(10);
+
+    Serial.println("Response:");
+    while(client.available()){
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+
+    Serial.println("\nClosing connection...\n");
 }
