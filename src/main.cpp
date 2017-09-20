4de0fa5eba17544e5aa5c34645ef464233540038
Nathan Manske <nathan.manske@utexas.edu>
2017-09-19 19:55:51 -0500

    Display stock symbol and price on Nokia5110

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
 
 void connect(void);
 String getStockSymbol(void);
 String getStockData(String symbol);
 float getCurrentPrice(String data);
+void setDisplay(String symbol, float price);
 
+#define D1_PIN 5
+#define D2_PIN 4
+#define D5_PIN 14
+#define D6_PIN 12
+#define D7_PIN 13
 #define RED_PIN 16
 #define GREEN_PIN 0
 #define BLUE_PIN 2
-#define BAUD_RATE 115200
+
+#define BAUD_RATE 9600
 #define HTTPS_PORT 443
 
 #define NUM_STOCK_SYMBOLS 4
@@ -22,6 +31,7 @@ static const char* host = "www.alphavantage.co";
 static const char* fingerprint = "3C B9 DA D3 0E 01 0F 53 EB B0 42 DD 39 73 44 9B 89 BD 1D BE";
 static const char* symbols[NUM_STOCK_SYMBOLS] = {"AMD", "AMZN", "SNAP", "VTTSX"};
 
+static Adafruit_PCD8544 display = Adafruit_PCD8544(D5_PIN, D7_PIN, D6_PIN, D1_PIN, D2_PIN);
 static uint8_t status = WL_IDLE_STATUS;
 static uint8_t symbol_index = 0;
 static float stock_prices[NUM_STOCK_SYMBOLS];
@@ -30,6 +40,9 @@ void setup(void) {
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
+
+    display.begin();
+    display.setContrast(50);
 
     ESP.wdtDisable();
     ESP.wdtEnable(WDTO_500MS);
@@ -46,6 +59,7 @@ void loop(void) {
     String symbol = getStockSymbol();
     String data = getStockData(symbol);
     float price = getCurrentPrice(data);
+    setDisplay(symbol, price);
 }
 
 void connect(void) {
@@ -139,4 +153,16 @@ float getCurrentPrice(String data) {
     Serial.println(price);
     Serial.println();
     return price;
+}
+
+void setDisplay(String symbol, float price) {
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.setTextSize(2);
+    display.setTextColor(BLACK);
+    display.println(symbol);
+    display.print("$");
+    display.println(price);
+    display.print("INCR");
+    display.display();
 }
